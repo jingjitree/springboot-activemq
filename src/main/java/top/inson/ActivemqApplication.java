d0b60ab01a512ce8001327cadf0917275637d83c
jingjitree <jingjitree@gmail.com>
2020-10-06 11:16:05 +0800

    TLOG日志框架集成测试

--- src/main/java/top/inson/ActivemqApplication.java
+++ src/main/java/top/inson/ActivemqApplication.java
@@ -1,11 +1,16 @@
 package top.inson;
 
 
+import com.yomahub.tlog.core.enhance.AspectLogEnhance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ActivemqApplication {
+
+    static {
+        AspectLogEnhance.enhance();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ActivemqApplication.class, args);
--- src/main/java/top/inson/web/HelloController.java
+++ src/main/java/top/inson/web/HelloController.java
@@ -1,13 +1,16 @@
 package top.inson.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
+        log.info("欢迎页面日志");
         return "ARE YOU OK?";
     }
 
